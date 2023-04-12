debc36bfa40804727498b5339f05657a2fb638ac
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-12 21:06:57 +0300

    Rename variables in colormask

--- Exercise 1/colormask.m
+++ Exercise 1/colormask.m
@@ -1,20 +1,20 @@
-function colormask = colormask(m,n,wbmults,align)
+function colormask = colormask(m, n, wb_coefficients, alignment)
 % Makes a white-balance multiplicative mask for an image of size m-by-n
-% with RGB while balance multipliers WBMULTS = [R_scale G_scale B_scale].
-% ALIGN is string indicating Bayer arrangement: ’rggb’,’gbrg’,’grbg’,’bggr’
-colormask = wbmults(2)*ones(m,n); %Initialize to all green values
-switch align
+% with RGB white balance multipliers: wb_coefficients = [R_scale G_scale B_scale].
+% alignment is a string indicating the Bayer arrangement
+colormask = wb_coefficients(2)*ones(m,n); % Initialize to all green values
+switch alignment
     case "bggr"
-        colormask(2:2:end,2:2:end) = wbmults(1); %r
-        colormask(1:2:end,1:2:end) = wbmults(3); %b
+        colormask(2:2:end,2:2:end) = wb_coefficients(1); %r
+        colormask(1:2:end,1:2:end) = wb_coefficients(3); %b
     case "gbrg"
-        colormask(2:2:end,1:2:end) = wbmults(1); %r
-        colormask(1:2:end,2:2:end) = wbmults(3); %b
+        colormask(2:2:end,1:2:end) = wb_coefficients(1); %r
+        colormask(1:2:end,2:2:end) = wb_coefficients(3); %b
     case "grbg"
-        colormask(1:2:end,2:2:end) = wbmults(1); %r
-        colormask(1:2:end,2:2:end) = wbmults(3); %b
+        colormask(1:2:end,2:2:end) = wb_coefficients(1); %r
+        colormask(1:2:end,2:2:end) = wb_coefficients(3); %b
     case "rggb"
-        colormask(1:2:end,1:2:end) = wbmults(1); %r
-        colormask(2:2:end,2:2:end) = wbmults(3); %b
+        colormask(1:2:end,1:2:end) = wb_coefficients(1); %r
+        colormask(2:2:end,2:2:end) = wb_coefficients(3); %b
 end
 end
