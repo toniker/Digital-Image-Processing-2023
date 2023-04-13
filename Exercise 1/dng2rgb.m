2f6cf5e043270b015b86583bff31ea5414db4e4b
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-13 21:13:11 +0300

    Update dng2rgb.m

--- Exercise 1/dng2rgb.m
+++ Exercise 1/dng2rgb.m
@@ -2,34 +2,30 @@ function [Csrgb, Clinear, Cxyz, Ccam] = dng2rgb(rawim, XYZ2Cam, wbcoeffs, bayert
 % Apply White balance
 mask = colormask(size(rawim,1), size(rawim,2), wbcoeffs, bayertype);
 color_balanced_im = rawim .* mask;
-% montage([color_balanced_im, rawim]);
+
+% Demosaic
+customRGB = custom_demosaic(color_balanced_im, bayertype);
+image(customRGB * 2);
+temp = uint16(color_balanced_im/max(color_balanced_im(:))*2^16);
+builtinRGB = double(demosaic(temp,"rggb"))/2^16;
 
 % Interpolation
+[rows, cols] = size(customRGB);
+interpolated_im = zeros(rows, cols);
 if (method == "linear")
-    [rows, cols] = size(color_balanced_im);
-    interpolated_im = zeros(rows, cols);
     for i = 2 : rows - 1
         for j = 2 : cols - 1
-            interpolated_im(i,j) = (color_balanced_im(i-1,j) + color_balanced_im(i+1,j) + color_balanced_im(i,j-1) + color_balanced_im(i,j+1)) / 4;
+            interpolated_im(i,j) = (customRGB(i-1,j) + customRGB(i+1,j) + customRGB(i,j-1) + customRGB(i,j+1)) / 4;
         end
     end
 elseif (method == "nearest")
-    [rows, cols] = size(color_balanced_im);
-    interpolated_im = zeros(rows, cols);
     for i = 2 : rows - 1
         for j = 2 : cols - 1
-            interpolated_im(i,j) = color_balanced_im(i-1,j);
+            interpolated_im(i,j) = customRGB(i-1,j);
         end
     end
 end
 
-% Demosaic
-customRGB = custom_demosaic(color_balanced_im, bayertype);
-montage(customRGB);
-% imshow(customRGB);
-temp = uint16(color_balanced_im/max(color_balanced_im(:))*2^16);
-builtinRGB = double(demosaic(temp,"rggb"))/2^16;
-montage([customRGB, builtinRGB]);
 % Color Space Transformation
 XYZ2RGB = [[+3.2406, -1.5372, -0.4986], [-0.9689, +1.8758, +0.0415], [+0.0557, -0.2040, +1.0570]];
 
