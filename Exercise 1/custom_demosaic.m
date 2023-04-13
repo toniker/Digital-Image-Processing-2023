2172228b7662df371a18bd5bf065d8452dcb8aec
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-13 20:38:59 +0300

    Improve custom demosaic algorithm for rggb

--- Exercise 1/custom_demosaic.m
+++ Exercise 1/custom_demosaic.m
@@ -1,5 +1,5 @@
 function RGB = custom_demosaic(input_image, bayertype)
-RGB = 0;
+RGB = "invalid bayertype";
 switch bayertype
     case "bggr"
         RGB = internal_bggr_demosaic(input_image);
@@ -17,12 +17,12 @@ function J = internal_rggb_demosaic(I)
 J = zeros(m,n,3);
 for i = 2:m-1
     for j = 2:n-1
-        if mod(i+j,2) == 0 % Green pixel
+        if (mod(i,2) == 0 && mod(j, 2) == 0) % Blue pixel
+            J(i,j,:) = [(I(i,j-1)+I(i,j+1))/2, (I(i-1,j)+I(i+1,j))/2, I(i,j)];
+        elseif (mod(i,2) == 0 || mod(j, 2) == 0) % Green pixel
+            J(i,j,:) = [(I(i-1,j)+I(i+1,j))/2, I(i,j), (I(i,j-1)+I(i,j+1))/2];
+        else % Red pixel
             J(i,j,:) = [I(i,j), (I(i-1,j)+I(i+1,j))/2, (I(i,j-1)+I(i,j+1))/2];
-        elseif mod(i,2) == 0 % Red pixel
-            J(i,j,:) = [(I(i,j-1)+I(i,j+1))/2, I(i,j), (I(i-1,j)+I(i+1,j))/2];
-        else % Blue pixel
-            J(i,j,:) = [(I(i-1,j)+I(i+1,j))/2, (I(i,j-1)+I(i,j+1))/2, I(i,j)];
         end
     end
 end
