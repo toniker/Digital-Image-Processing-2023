9f88533e3e9b52d340193b4768cdf4afe1e3f319
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-17 15:28:58 +0300

    Set file name by the method of its decoding

--- Exercise 1/demo.m
+++ Exercise 1/demo.m
@@ -2,14 +2,14 @@ filename = "RawImage.tiff";
 [rawim, XYZ2Cam, wbcoeffs] = readdng(filename);
 
 bayertype = "rggb";
-method = "linear";
+method = "nearest";
 [Csrgb, Clinear, Cxyz, Ccam] = dng2rgb(rawim, XYZ2Cam, wbcoeffs, bayertype, method);
 
 % Write image files from the dng2rgb output
-imwrite(Csrgb, "output_rgb.jpg");
-imwrite(Clinear, "output_linear.jpg");
-imwrite(Cxyz, "output_xyz.jpg");
-imwrite(Ccam, "output_cam.jpg");
+imwrite(Csrgb, method+"_"+bayertype+"_"+"rgb.jpg");
+imwrite(Clinear, method+"_"+bayertype+"_"+"linear.jpg");
+imwrite(Cxyz, method+"_"+bayertype+"_"+"xyz.jpg");
+imwrite(Ccam, method+"_"+bayertype+"_"+"cam.jpg");
 
 % Create histograms for R, G, B
 hold on
