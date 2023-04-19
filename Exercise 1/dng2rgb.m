31095a15ddf460bfa886915caa3869eddf7d82fc
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-19 21:31:01 +0300

    Add HSV boost that matches the sample image for RGGB and linear interpolation

--- Exercise 1/dng2rgb.m
+++ Exercise 1/dng2rgb.m
@@ -11,6 +11,15 @@ XYZ2RGB = [[+3.2406, -1.5372, -0.4986], [-0.9689, +1.8758, +0.0415], [+0.0557, -
 
 Clinear = customRGB;
 Csrgb = Clinear .^ (1/2.2);
+Csrgb = real(Csrgb);
 Cxyz = apply_cmatrix(customRGB, XYZ2RGB);
 Ccam = apply_cmatrix(customRGB, XYZ2Cam);
+
+hsv = rgb2hsv(Csrgb);
+hsv(:,:,1) = hsv(:,:,1) * 1.15;
+hsv(:,:,2) = hsv(:,:,2) * 1.65;
+% hsv(:,:,3) = hsv(:,:,3) * 1.2;
+rgb = hsv2rgb(hsv);
+imwrite(rgb, "test.jpg");
+
 end
