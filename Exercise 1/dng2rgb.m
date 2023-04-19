fe111a147dba293f6d78c753df487c1bd04d9034
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-19 23:59:12 +0300

    Add comments

--- Exercise 1/dng2rgb.m
+++ Exercise 1/dng2rgb.m
@@ -1,25 +1,34 @@
 function [Csrgb, Clinear, Cxyz, Ccam] = dng2rgb(rawim, XYZ2Cam, wbcoeffs, bayertype, method)
-% Apply White balance
+%% White balance
 mask = colormask(size(rawim,1), size(rawim,2), wbcoeffs, bayertype);
 color_balanced_im = rawim .* mask;
 
-% Demosaic
-customRGB = custom_demosaic(color_balanced_im, bayertype, method);
+% Add built in demosaic for comparison
+imwrite((double(demosaic(uint16(color_balanced_im * 65535), bayertype)) / 65535) .^(1/2.2), "builtInDemosaic.jpg");
+
+%% Demosaic
+Clinear = custom_demosaic(color_balanced_im, bayertype, method);
+
+%% Color Space
+XYZ2RGB = [[+3.2406; -1.5372; -0.4986], [-0.9689; +1.8758; +0.0415], [+0.0557; -0.2040; +1.0570]];
+RGB2XYZ = inv(XYZ2RGB);
+
+% Gamma Correction
+Csrgb = real(Clinear .^ (1/2.2));
+
+% Convert to Hue, Saturation, Value
+hsv = rgb2hsv(Csrgb);
+
+% Hue shift
+hsv(:,:,1) = hsv(:,:,1) * 1.15;
+% Saturation boost
+hsv(:,:,2) = hsv(:,:,2) * 1.65;
+
+% Convert to RGB
+Csrgb = hsv2rgb(hsv);
 
 % Color Space Transformation
-XYZ2RGB = [[+3.2406, -1.5372, -0.4986], [-0.9689, +1.8758, +0.0415], [+0.0557, -0.2040, +1.0570]];
-
-Clinear = customRGB;
-Csrgb = Clinear .^ (1/2.2);
-Csrgb = real(Csrgb);
-Cxyz = apply_cmatrix(customRGB, XYZ2RGB);
-Ccam = apply_cmatrix(customRGB, XYZ2Cam);
-
-hsv = rgb2hsv(Csrgb);
-hsv(:,:,1) = hsv(:,:,1) * 1.15;
-hsv(:,:,2) = hsv(:,:,2) * 1.65;
-% hsv(:,:,3) = hsv(:,:,3) * 1.2;
-rgb = hsv2rgb(hsv);
-imwrite(rgb, "test.jpg");
+Cxyz = apply_cmatrix(Clinear, RGB2XYZ);
+Ccam = apply_cmatrix(Cxyz, XYZ2Cam);
 
 end
