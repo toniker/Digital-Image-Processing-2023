0c0906056da13251b7b3e4542d3d358e5a82583b
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-21 23:12:41 +0300

    Move stuff around

--- Exercise 1/dng2rgb.m
+++ Exercise 1/dng2rgb.m
@@ -3,11 +3,11 @@ function [Csrgb, Clinear, Cxyz, Ccam] = dng2rgb(rawim, XYZ2Cam, wbcoeffs, bayert
 mask = colormask(size(rawim,1), size(rawim,2), wbcoeffs, bayertype);
 color_balanced_im = rawim .* mask;
 
-% Add built in demosaic for comparison
-imwrite((double(demosaic(uint16(color_balanced_im * 65535), bayertype)) / 65535) .^(1/2.2), "builtInDemosaic.jpg");
-
 %% Demosaic
 Clinear = custom_demosaic(color_balanced_im, bayertype, method);
+
+% Use built in demosaic for comparison
+imwrite((double(demosaic(uint16(color_balanced_im * 65535), bayertype)) / 65535) .^(1/2.2), "builtInDemosaic.jpg");
 
 %% Color Space
 XYZ2RGB = [[+3.2406; -1.5372; -0.4986], [-0.9689; +1.8758; +0.0415], [+0.0557; -0.2040; +1.0570]];
