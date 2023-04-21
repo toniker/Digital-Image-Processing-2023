5d47925a90eda11899246df1a642266c70bd7449
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-21 14:50:24 +0300

    Update bilinear_grbg_demosaic

--- Exercise 1/custom_demosaic.m
+++ Exercise 1/custom_demosaic.m
@@ -131,14 +131,24 @@ alpha = 4/8;
 beta = 5/8;
 gamma = 6/8;
 
-for i = 2:m-1
-    for j = 2:n-1
-        if mod(i+j,2) == 0 % Green pixel
-            J(i,j,:) = [(I(i,j-1)+I(i,j+1))/2, I(i,j), (I(i-1,j)+I(i+1,j))/2];
+for i = 3:m-2
+    for j = 3:n-2
+        if (mod(i+j,2) == 0 && mod(i,2 == 0)) % Green pixel in blue row
+            red_pixel = beta * (I(i,j) + 4/5 * (I(i-1,j) + I(i+1,j)) - 1/5 * (I(i-1,j-1) + I(i-1,j+1) + I(i+1,j+1) + I(i+1,j-1) + I(i-2,j) + I(i+2,j)) + 1/10 * I(i,j-2) + 1/10 * I(i,j+2));
+            blue_pixel = beta * (I(i,j) + 4/5 * (I(i-1,j) + I(i+1,j)) - 1/5 * (I(i-1,j-1) + I(i-1,j+1) + I(i+1,j+1) + I(i+1,j-1) + I(i-2,j) + I(i+2,j)) + 1/10 * I(i,j-2) + 1/10 * I(i,j+2));
+            J(i,j,:) = [red_pixel, I(i,j), blue_pixel];
+        elseif (mod(i+j,2) == 0 && mod(i,2 == 0)) % Green pixel in red row
+            red_pixel = beta * (I(i,j) + 4/5 * (I(i,j-1) + I(i,j+1)) - 1/5 * (I(i-1,j-1) + I(i-1,j+1) + I(i+1,j+1) + I(i+1,j-1) + I(i,j-2) + I(i,j+2)) + 1/10 * I(i-2,j) + 1/10 * I(i+2,j));
+            blue_pixel = beta * (I(i,j) + 4/5 * (I(i,j-1) + I(i,j+1)) - 1/5 * (I(i-1,j-1) + I(i-1,j+1) + I(i+1,j+1) + I(i+1,j-1) + I(i,j-2) + I(i,j+2)) + 1/10 * I(i-2,j) + 1/10 * I(i+2,j));
+            J(i,j,:) = [red_pixel, I(i,j), blue_pixel];
         elseif mod(j,2) == 0 % Red pixel
-            J(i,j,:) = [I(i,j), (I(i-1,j)+I(i+1,j)+I(i,j-1)+I(i,j+1))/4, (I(i,j-1)+I(i,j+1))/2];
+            green_pixel = (I(i-1,j)+I(i+1,j)+I(i,j-1)+I(i,j+1))/4 + alpha * (I(i,j) - 1/4 * (I(i,j-2)+I(i,j+2)+I(i-2,j)+I(i+2,j)));
+            blue_pixel = gamma * (I(i,j) + 1/3 * (I(i-1,j-1) + I(i-1,j+1) + I(i+1,j+1) + I(i+1,j-1)) - 1/4 * (I(i-2,j) + I(i+2,j) + I(i,j-2) + I(i,j+2)));
+            J(i,j,:) = [I(i,j), green_pixel, blue_pixel];
         else % Blue pixel
-            J(i,j,:) = [(I(i-1,j)+I(i+1,j))/2, (I(i-1,j)+I(i+1,j)+I(i,j-1)+I(i,j+1))/4, I(i,j)];
+            green_pixel = (I(i-1,j)+I(i+1,j)+I(i,j-1)+I(i,j+1))/4 + alpha * (I(i,j) - 1/4 * (I(i,j-2)+I(i,j+2)+I(i-2,j)+I(i+2,j)));
+            red_pixel = gamma * (I(i,j) + 1/3 * (I(i-1,j-1) + I(i-1,j+1) + I(i+1,j+1) + I(i+1,j-1)) - 1/4 * (I(i-2,j) + I(i+2,j) + I(i,j-2) + I(i,j+2)));
+            J(i,j,:) = [red_pixel, green_pixel, I(i,j)];
         end
     end
 end
