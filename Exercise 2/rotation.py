49c914ec8d42cc04d6268ae884ca882eecb06b87
Antonis Keremidis <tkeremidis@gmail.com>
2023-05-18 20:32:14 +0300

    Add test function for rotation + documentation

--- Exercise 2/rotation.py
+++ Exercise 2/rotation.py
@@ -4,11 +4,13 @@ import cv2
 import numpy as np
 
 
-def find_rotation_angle(image: np.ndarray):
+def find_rotation_angle_hough(image):
     """
-    This function finds the rotation angle of the image.
+    Uses a Hough transform to find the rotation angle of the image.
+    Using the DFT of the image, we find the lines corresponding to the text and use the slope of those lines to find the
+    angle of the text.
     :param image: The input image
-    :return: the angle as a float
+    :return: The estimated rotation angle
     """
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -30,7 +32,7 @@ def find_rotation_angle(image: np.ndarray):
     lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 20, minLineLength=70, maxLineGap=20)
 
     if lines is None:
-        return "unknown"
+        return None
 
     angles = []
     # Draw lines on the image
@@ -43,12 +45,60 @@ def find_rotation_angle(image: np.ndarray):
         angles.append(predicted_angle)
         cv2.line(magnitude_spectrum, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    cv2.imwrite("lines.jpg", magnitude_spectrum)
+    cv2.imwrite("hough_lines_on_magnitude_spectrum.jpg", magnitude_spectrum)
 
     return -round(float(np.mean(angles)), 2)
 
 
+def find_rotation_angle(image):
+    """
+    Uses the methodology described in the homework to find the rotation angle of the image.
+    Using the DFT of the image, we find the angle of the maximum frequency, to the center of the image. With that
+    initial estimate, we rotate the image and use the vertical projection until we find the angle that gives us the most
+    shifts between bright and dark areas.
+    :param image: The input image
+    :return: The estimated angle
+    """
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred_image = cv2.blur(image, (8, 8))
+
+    f = np.fft.fft2(blurred_image)
+    f_shift = np.fft.fftshift(f)
+    magnitude_spectrum = 20 * np.log(np.abs(f_shift))
+
+    center = tuple(i // 2 for i in magnitude_spectrum.shape)
+    magnitude_spectrum[center] = 0
+
+    max_frequency = np.argmax(magnitude_spectrum)
+    max_frequency_position = np.unravel_index(max_frequency, magnitude_spectrum.shape)
+
+    initial_estimation = np.rad2deg(
+        np.arctan2(center[1] - max_frequency_position[1], center[0] - max_frequency_position[0]))
+    initial_estimation = int(initial_estimation)
+    initial_estimation %= 90
+
+    best_estimate = initial_estimation, 0
+    for possible_angle in range(initial_estimation - 20, initial_estimation + 20):
+        test_image = fast_rotate_image(image, -possible_angle)
+        test_image = cv2.blur(test_image, (11, 11))
+        test_image[test_image < 240] = 0
+        test_image[test_image >= 240] = 255
+        vertical_projection = np.sum(test_image, axis=0)
+        diffs = [abs(vertical_projection[j + 1] - vertical_projection[j]) for j in range(len(vertical_projection) - 1)]
+        avg_diff = np.mean(diffs)
+        if avg_diff > best_estimate[1]:
+            best_estimate = (possible_angle, avg_diff)
+
+    return best_estimate[0]
+
+
 def rotate_image(input_image, angle):
+    """
+    Rotates the image by the given angle.
+    :param input_image: The image to be rotated.
+    :param angle: The angle to rotate the image.
+    :return: The rotated image.
+    """
     radians = np.radians(angle)
     input_height, input_width, number_of_colors = np.shape(input_image)
     input_center = (input_height // 2, input_width // 2)
@@ -73,6 +123,13 @@ def rotate_image(input_image, angle):
 
 
 def fast_rotate_image(image, angle):
+    """
+    Uses OpenCV to rotate the image.
+    This function was created since it is orders of magnitude faster than the rotate_image function.
+    :param image: The image to be rotated.
+    :param angle: The angle to rotate the image.
+    :return: The rotated image.
+    """
     height, width = image.shape[:2]
     image_center = (width // 2, height // 2)
 
@@ -100,8 +157,8 @@ if __name__ == "__main__":
 
     rot_image = fast_rotate_image(img, 0)
 
-    angle = find_rotation_angle(rot_image)
-    print(f"Angle: {angle}")
+    angle = find_rotation_angle_hough(rot_image)
+    print(f"Estimated angle: {angle} degrees")
 
     # Measure the execution time
     execution_time = round(time.time() - start_time, 3)
