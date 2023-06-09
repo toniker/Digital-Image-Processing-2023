68c748737f2928f291d592632feb47ff05ed9e9a
Antonis Keremidis <tkeremidis@gmail.com>
2023-06-09 13:44:22 +0300

    Populate descriptor_matching, ransac and stich functions

--- Exercise 3/stitch.py
+++ Exercise 3/stitch.py
@@ -114,26 +114,109 @@ def my_detect_harris_features(I):
 
 
 def descriptor_matching(points_1, points_2, percentage_threshold):
-    distances = np.array((len(points_1), len(points_2)))
+    distances = np.empty((len(points_1), len(points_2)))
+    matching_points = []
+
     for index_1, point_1 in enumerate(points_1):
         for index_2, point_2 in enumerate(points_2):
-            distance = euclidean_distance(point_1, point_2)
-            distances[index_1, index_2] = distance
+            point_1 = np.array(point_1)
+            point_2 = np.array(point_2)
+            distances[index_1, index_2] = np.linalg.norm(point_1 - point_2)
 
-    for index in distances:
-        pass
+    np.save("distances.npy", distances)
+    distances = np.load("distances.npy")
+
+    for index_1, point_1 in enumerate(points_1):
+        minimum_value_index = np.argmin(distances[index_1])
+        matching_points.append((index_1, minimum_value_index))
+
+    _matching_points = [x for x in matching_points if x[1] != 0]
+    matching_points = _matching_points
+
     return matching_points
 
 
 def my_RANSAC(matching_points, r, N):
+    inlier_matching_points = []
+    outlier_matching_points = []
+    theta, d = None, None
+
+    for i in range(N):
+        random_points = random.sample(matching_points, 2)
+        point_1, point_2 = random_points[0], random_points[1]
+        point_1 = point_1[0]
+        point_2 = point_2[0]
+
+        y1, x1 = point_1
+        y2, x2 = point_2
+
+        theta = np.arctan((y2 - y1) / (x2 - x1))
+        d = y1 - x1 * (y2 - y1) / (x2 - x1)
+
+        inlier_matching_points = []
+        outlier_matching_points = []
+        for point in matching_points:
+            y, x = point[0]
+            distance = np.abs(y - x * np.tan(theta) - d)
+            if distance < r:
+                inlier_matching_points.append(point)
+            else:
+                outlier_matching_points.append(point)
+
+        if len(inlier_matching_points) > len(outlier_matching_points):
+            break
+
     H = {'theta': theta, 'd': d}
-    return (H, inlier_matching_points, outlier_matching_points)
+    return H, inlier_matching_points, outlier_matching_points
 
 
 def my_stitch(im1, im2):
-    im1_harris = my_detect_harris_features(im1)
-    im2_harris = my_detect_harris_features(im2)
+    grey_im1 = cv2.cvtColor(im1, cv2.COLOR_RGB2GRAY)
+    grey_im2 = cv2.cvtColor(im2, cv2.COLOR_RGB2GRAY)
 
+    grey_im1_float = grey_im1.astype(np.float32) / np.max(grey_im1)
+    grey_im2_float = grey_im2.astype(np.float32) / np.max(grey_im2)
+
+    im1_data = {'image': im1, 'grey_image': grey_im1, 'corners': my_detect_harris_features(grey_im1_float)}
+    im2_data = {'image': im2, 'grey_image': grey_im2, 'corners': my_detect_harris_features(grey_im2_float)}
+
+    rho_m = 5
+    rho_M = 20
+    rho_step = 1
+    N = 8
+
+    im1_data['descriptors'] = []
+    for corner in im1_data['corners']:
+        im1_data['descriptors'].append(my_local_descriptor(grey_im1, corner, rho_m, rho_M, rho_step, N))
+
+    im2_data['descriptors'] = []
+    for corner in im2_data['corners']:
+        im2_data['descriptors'].append(my_local_descriptor(grey_im2, corner, rho_m, rho_M, rho_step, N))
+
+    data = {'im1': im1_data, 'im2': im2_data}
+    np.save('data_with_descriptors.npy', data)
+    # data = np.load('data_with_descriptors.npy', allow_pickle=True).tolist()
+    # im1_data = data['im1']
+    # im2_data = data['im2']
+    #
+    # matching_points = descriptor_matching(im1_data['descriptors'], im2_data['descriptors'], percentage_threshold=0.8)
+    #
+    # comb = cv2.imread("combined.png")
+    # np.random.shuffle(matching_points)
+    # for i in range(len(matching_points) // 20):
+    #     matching_point = matching_points[i]
+    #     index_1, index_2 = matching_point
+    #     (y1, x1) = im1_data['corners'][index_1]
+    #     (y2, x2) = im2_data['corners'][index_2]
+    #     x2 += 1360
+    #     cv2.line(comb, (x1, y1), (x2, y2), (0, 255, 0))
+    # cv2.imwrite("combined_lines.jpg", comb)
+    #
+    # r = 5
+    # N = 100
+    # H, inlier_matching_points, outlier_matching_points = my_RANSAC(matching_points, r, N)
+    #
+    # print("theta: ", H['theta'], "d: ", H['d'])
     return stitched
 
 
